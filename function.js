629339625ff4caee661c75ab8cadc4f738e70cda
Guido Gatica <gdgatica@gmail.com>
2023-03-12 10:00:11 -0300

    Ultimos ajustes

--- function.js
+++ function.js
@@ -1,6 +1,15 @@
+const botonInfo = document.getElementById("botonInfo");
+const parrafoOculto = document.getElementById("parrafoOculto");
+const textoBoton = document.getElementById("textoBoton");
+const contenedorBoton = document.getElementById("botonInfo").parentNode;
+contenedorBoton.classList.add("text-center");
 
-document.getElementById("botonInfo").addEventListener('click', function() {
-
-    document.getElementById("parrafoOculto").style.display = 'block';
-
+botonInfo.addEventListener("click", function() {
+  if (parrafoOculto.style.display === "none") {
+    parrafoOculto.style.display = "block";
+    textoBoton.innerHTML = "Ocultar";
+  } else {
+    parrafoOculto.style.display = "none";
+    textoBoton.innerHTML = "Información Personal <p id='clickAqui'>click aquí</p>";
+  }
 });
